b43fd717f13e4dd459a1f9de831f25e6f7fed644
Roberto C Melchor <rmelchor151@gmail.com>
2021-01-22 11:00:54 -0500

    add association to student and campus models

--- database/models/index.js
+++ database/models/index.js
@@ -1,21 +1,14 @@
 // Here, we can prepare to register our models, set up associations between tables, and generate a barrel file for the models;
 
-<<<<<<< HEAD
 //export student model 
 const Student = require('./student')
-
-module.exports = {
-  Student
-=======
-
+//export campus model
 const Campus = require("./campus")
-// const Student = require("./student")
 
-// Campus.hasMany(Student);
-// Student.belongsTo(Campus);
+Campus.hasMany(Student);
+Student.belongsTo(Campus);
 
 module.exports = {
-  Campus,
-  // Student
->>>>>>> main
+  Student,
+  Campus
 };
